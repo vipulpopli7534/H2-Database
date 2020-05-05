9938468afdb94c403cfc02878632d6f3b81974b6
Vipul Popli <vipul.popli@cleartrip.com>
2020-05-05 21:43:04 +0530

    added author template thymleaf

--- src/main/java/com/vipul/springframework/spring5web/controllers/ModelController.java
+++ src/main/java/com/vipul/springframework/spring5web/controllers/ModelController.java
@@ -1,21 +1,30 @@
 package com.vipul.springframework.spring5web.controllers;
 
+import com.vipul.springframework.spring5web.dataservice.AuthorService;
 import com.vipul.springframework.spring5web.dataservice.BookService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class BookController {
+public class ModelController {
     private final BookService bookService;
+    private final AuthorService authorService;
 
-    public BookController(BookService bookService) {
+    public ModelController(BookService bookService, AuthorService authorService) {
         this.bookService = bookService;
+        this.authorService = authorService;
     }
 
     @RequestMapping("/getBooks")
     public String getBooks(Model model){
         model.addAttribute("books", bookService.findAll());
         return "books";
+    }
+
+    @RequestMapping("/getAuthors")
+    public String getAuthors(Model model){
+        model.addAttribute("authors", authorService.findAll());
+        return "authors";
     }
 }
